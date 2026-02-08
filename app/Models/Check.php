fd2c474bc06fdce4cd3186f44ef0054c7b8b6e02
Ezequiel Tzofeheer <contato@ezequieltzofeheer.com.br>
2026-02-08 12:19:44 -0300

    Check - (Model and Migration) [Created]

---
+++ app/Models/Check.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Check extends Model
+{
+    /**
+     * Define que a chave primária não é auto-incremental.
+     */
+    public $incrementing = false;
+
+    /**
+     * Define que a chave primária é do tipo string (UUID).
+     */
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'endpoint_id', 'status_code', 'response_body',
+    ];
+}
